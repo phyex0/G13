b3df2a2cd1d90d814a479072ea1301bdc870a693
phyex0 <phyex0-halit_burak@outlook.com>
2020-11-22 21:42:01 +0300

    Person.java oluşturuldu
    Müşterinin sahip olabaileceği veriler ve bunları tutmak ile alakalı işlemler yapılacak.

---
+++ src/g13/Person.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g13;
+
+/**
+ This class must contain clients data
+ */
+public class Person {
+    
+    private String clientName="";
+    private String clientPhoneNumber="";
+    private double clientStar=0;
+    
+    
+    
+}

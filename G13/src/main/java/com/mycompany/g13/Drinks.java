e8af7cfc2e530f4c2d8f7e0295e4433f2f32b7ab
phyex0 <phyex0-halit_burak@outlook.com>
2021-01-01 16:28:16 +0300

    1

--- G13/src/main/java/com/mycompany/g13/Drinks.java
+++ G13/src/main/java/com/mycompany/g13/Drinks.java
@@ -76,6 +76,11 @@ public  class Drinks extends Products implements Sizeable {
 
     @Override
     public void setDrinkFeatures(String size, String milk, String extras) {
-        if(getType().equalsIgnoreCase("Şişelenmiş"))
+        if(getType().equalsIgnoreCase("Şişelenmiş")){
+            setSize(size);
+            setMilk(milk);
+            setSyrup(extras);
+        
+        }
     }
 }
